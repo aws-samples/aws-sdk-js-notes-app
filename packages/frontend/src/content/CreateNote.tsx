1d58854ebaa0b5912098c1587dbab4b2b9c48861
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-02-02 19:35:11 -0800

    chore: disable form input when recording

--- packages/frontend/src/content/CreateNote.tsx
+++ packages/frontend/src/content/CreateNote.tsx
@@ -56,6 +56,7 @@ const CreateNote = (props: RouteComponentProps) => {
                 setNoteContent(content);
               }
             }}
+            disabled={isRecording}
           />
         </Form.Group>
         <RecordAudioButton
