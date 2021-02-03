871ae1cba3dc62f64b20a2c2565ace0dd0f80d1d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-02-02 22:41:33 -0800

    chore(infra): populate region in CfnOutput

--- packages/infra/cdk/aws-sdk-js-notes-app-stack.ts
+++ packages/infra/cdk/aws-sdk-js-notes-app-stack.ts
@@ -113,6 +113,7 @@ export class AwsSdkJsNotesAppStack extends cdk.Stack {
       },
     });
 
+    new cdk.CfnOutput(this, "Region", { value: this.region });
     new cdk.CfnOutput(this, "GatewayUrl", { value: api.url });
     new cdk.CfnOutput(this, "IdentityPoolId", { value: identityPool.ref });
     new cdk.CfnOutput(this, "FilesBucket", { value: filesBucket.bucketName });
