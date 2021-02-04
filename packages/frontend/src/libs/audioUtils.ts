db53180cafbb2c099c0356242203b45eeb0a1363
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-03 19:50:29 -0800

    chore: clean-up pcmEncode utility function (#16)

--- packages/frontend/src/libs/audioUtils.ts
+++ packages/frontend/src/libs/audioUtils.ts
@@ -1,10 +1,9 @@
 export const pcmEncode = (input: any) => {
-  var offset = 0;
-  var buffer = new ArrayBuffer(input.length * 2);
-  var view = new DataView(buffer);
-  for (var i = 0; i < input.length; i++, offset += 2) {
-    var s = Math.max(-1, Math.min(1, input[i]));
-    view.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+  const buffer = new ArrayBuffer(input.length * 2);
+  const view = new DataView(buffer);
+  for (let i = 0; i < input.length; i++) {
+    const s = Math.max(-1, Math.min(1, input[i]));
+    view.setInt16(i * 2, s < 0 ? s * 0x8000 : s * 0x7fff, true);
   }
   return buffer;
 };
