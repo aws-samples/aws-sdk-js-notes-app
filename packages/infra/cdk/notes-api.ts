3bd653ab5abc4ca0c81ce8846b1bfeebc41fe928
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-10 08:32:28 -0800

    feat(backend): use nodejs14.x runtime in lambdas (#23)

--- packages/infra/cdk/notes-api.ts
+++ packages/infra/cdk/notes-api.ts
@@ -19,7 +19,7 @@ export class NotesApi extends cdk.Construct {
     const { table, grantActions } = props;
 
     this.handler = new lambda.Function(this, "handler", {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      runtime: lambda.Runtime.NODEJS_14_X,
       handler: "app.handler",
       // ToDo: find a better way to pass lambda code
       code: lambda.Code.fromAsset(`../backend/dist/${id}`),
