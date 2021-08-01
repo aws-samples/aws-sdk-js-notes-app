bc39f3e223a4c795aa516e7f6102c6c3f10e46b7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-31 22:44:00 -0700

    chore: add buffer

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,9 +1,14 @@
+import { Buffer } from "buffer";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Routes } from "./Routes";
 
 if (typeof (window as any).global === "undefined") {
   (window as any).global = window;
+}
+
+if (typeof (window as any).Buffer === "undefined") {
+  (window as any).Buffer = Buffer;
 }
 
 ReactDOM.render(
