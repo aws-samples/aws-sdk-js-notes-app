8ca364db1fa9472da619b254e731c27d318d8cba
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-31 17:20:58 -0700

    fix: manually add global polyfill
    Refs: https://github.com/bevacqua/dragula/issues/602#issuecomment-477734315

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Routes } from "./Routes";
 
+if (typeof (window as any).global === "undefined") {
+  (window as any).global = window;
+}
+
 ReactDOM.render(
   <div className="container" style={{ height: "100vh" }}>
     <Routes />
