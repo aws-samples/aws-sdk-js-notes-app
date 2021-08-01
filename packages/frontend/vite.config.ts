9f1c9f1a4be48cd3aabb583350e83019a908528a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-31 23:22:14 -0700

    fix: resolve.alias for runtimeConfig
    Refs: https://github.com/aws-amplify/amplify-js/issues/7499#issuecomment-808503506

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,4 +4,9 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
 });
