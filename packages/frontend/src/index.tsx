a01b49d8c825282d732ff421f7bc21b1cb8fd1a8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-31 23:16:24 -0700

    fix: add provess polyfill

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,14 +1,18 @@
 import { Buffer } from "buffer";
+import process from "process";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Routes } from "./Routes";
 
+// Polyfills required for MicrophoneStream
 if (typeof (window as any).global === "undefined") {
   (window as any).global = window;
 }
-
 if (typeof (window as any).Buffer === "undefined") {
   (window as any).Buffer = Buffer;
+}
+if (typeof (window as any).process === "undefined") {
+  (window as any).process = process;
 }
 
 ReactDOM.render(
