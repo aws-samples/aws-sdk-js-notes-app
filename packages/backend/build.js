e98c0f5e0de96d31f9663d98c4bd0303398373a9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-08 22:17:17 -0700

    chore(backend): add build script for esbuild

---
+++ packages/backend/build.js
@@ -0,0 +1,15 @@
+const glob = require("glob");
+const { buildSync } = require("esbuild");
+
+const entryPoints = glob.sync("./src/*.ts");
+
+buildSync({
+  entryPoints,
+  entryNames: "[name]/app",
+  bundle: true,
+  minify: true,
+  outdir: "dist",
+  platform: "node",
+  target: "node14",
+  logLevel: "info",
+});
