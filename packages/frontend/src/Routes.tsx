25dc5188a0d828964be0aff8599adabf01447a83
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-14 22:46:42 -0800

    chore(frontend): update <Router> elements to <Routes>

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-import { Router } from "@reach/router";
+import { Routes } from "react-router-dom";
 
 const ListNotes = lazy(() => import("./content/ListNotes"));
 const CreateNote = lazy(() => import("./content/CreateNote"));
@@ -9,12 +9,12 @@ const NotFound = lazy(() => import("./content/NotFound"));
 const Routes = () => (
   <div className="mt-md-4 d-flex flex-column justify-content-center">
     <Suspense fallback={<div>Loading...</div>}>
-      <Router>
+      <Routes>
         <ListNotes path="/" />
         <CreateNote path="/note/new" />
         <ShowNote path="/notes/:noteId" />
         <NotFound default />
-      </Router>
+      </Routes>
     </Suspense>
   </div>
 );
