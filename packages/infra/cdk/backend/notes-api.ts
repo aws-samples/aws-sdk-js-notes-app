0432aa731a9043fcc17c82d0b5a39cfcdc22be24
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-03-29 10:35:13 -0700

    Use aws_cdk.aws_lambda_nodejs for deploying lambda functions (#122)

--- packages/infra/cdk/backend/notes-api.ts
+++ packages/infra/cdk/backend/notes-api.ts
@@ -1,4 +1,7 @@
-import { aws_dynamodb as dynamodb, aws_lambda as lambda } from "aws-cdk-lib";
+import {
+  aws_dynamodb as dynamodb,
+  aws_lambda_nodejs as lambda,
+} from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export interface NotesApiProps {
@@ -10,21 +13,15 @@ export interface NotesApiProps {
 
 export class NotesApi extends Construct {
   /** allows accessing the counter function */
-  public readonly handler: lambda.Function;
+  public readonly handler: lambda.NodejsFunction;
 
   constructor(scope: Construct, id: string, props: NotesApiProps) {
     super(scope, id);
 
     const { table, grantActions } = props;
 
-    this.handler = new lambda.Function(this, "handler", {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "app.handler",
-      // ToDo: find a better way to pass lambda code
-      code: lambda.Code.fromAsset(`../backend/dist/${id}`),
-      environment: {
-        NOTES_TABLE_NAME: table.tableName,
-      },
+    this.handler = new lambda.NodejsFunction(this, id, {
+      environment: { NOTES_TABLE_NAME: table.tableName },
     });
 
     // grant the lambda role read/write permissions to notes table
