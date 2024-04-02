1588212ec4d6faff28f7f974565ae769032cf60f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-04-01 18:05:49 -0700

    chore(deps): remove microphone-stream and polyfills

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,19 +1,6 @@
-import { Buffer } from "buffer";
-import process from "process";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Routes } from "./Routes";
-
-// Polyfills required for MicrophoneStream
-if (typeof (window as any).global === "undefined") {
-  (window as any).global = window;
-}
-if (typeof (window as any).Buffer === "undefined") {
-  (window as any).Buffer = Buffer;
-}
-if (typeof (window as any).process === "undefined") {
-  (window as any).process = process;
-}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
